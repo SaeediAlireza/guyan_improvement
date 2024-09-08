8e7814669837df25ad186e8eece65205dc6aa060
SaeediAlireza <alirexasaeedi@gmail.com>
2024-09-08 15:39:10 +0330

    adding csv sender

--- routers/phone_number.py
+++ routers/phone_number.py
@@ -1,11 +1,12 @@
 from typing import Annotated, List
 from fastapi import APIRouter, Depends, HTTPException, status, Response
 
-# from repository import user_type
+from fastapi.responses import StreamingResponse
 from model import model, schemas
 from sqlalchemy.orm import Session
 from util import util
-
+import pandas as pd
+import io
 
 router = APIRouter(tags=["phone number"], prefix="/phone-numbers")
 
@@ -49,6 +50,36 @@ def get_phone_number_by_id(
     if not user_type:
         response.status_code = status.HTTP_404_NOT_FOUND
     return user_type
+
+
+@router.get("csv")
+def get_phone_numbers_csv(response: Response, db: Session = Depends(util.get_db)):
+    # Query all phone numbers
+    phone_numbers = db.query(model.PhoneNumber).all()
+
+    if not phone_numbers:
+        response.status_code = status.HTTP_404_NOT_FOUND
+
+    df = pd.DataFrame(
+        [
+            {
+                "id": pn.id,
+                "number": pn.number,
+                "phone_number_owner_id": pn.phone_number_owner_id,
+            }
+            for pn in phone_numbers
+        ]
+    )
+
+    csv_buffer = io.StringIO()
+    df.to_csv(csv_buffer, index=False)
+    csv_buffer.seek(0)
+
+    return StreamingResponse(
+        iter([csv_buffer.getvalue()]),
+        media_type="text/csv",
+        headers={"Content-Disposition": "attachment; filename=phone_numbers.csv"},
+    )
 
 
 @router.put("update")
